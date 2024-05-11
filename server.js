294f8adb74217c78a2aed3a190c9e2f48b44a9a1
Toroyan <toroyaninesa@gmail.com>
2024-05-11 22:05:00 +0400

    env test

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('src/environments/environment').environment.gptAuthHeader = process.env.gptAuthHeader;
 app.use(express.static(__dirname + '/dist/purplecube'));
 app.get('/*', function(req,res) {
+
   res.sendFile(path.join(__dirname+
     '/dist/purplecube/index.html'));});
 app.listen(process.env.PORT || 8080);
