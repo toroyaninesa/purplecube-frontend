d3d94a4468e0e0fbc0cc198226305b2cbc29de02
ovorskanyan <ovorskanyan@griddynamics.com>
2024-05-18 17:10:53 +0400

    refactor: add switchMap

--- src/app/modules/company/open-positions/applicants/applicants.component.ts
+++ src/app/modules/company/open-positions/applicants/applicants.component.ts
@@ -5,6 +5,7 @@ import {ActivatedRoute} from '@angular/router';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {NavigationService} from '../../../../core/navigation/navigation.service';
+import {switchMap} from 'rxjs';
 
 @Component({
     selector: 'app-applicants',
@@ -38,33 +39,37 @@ export class ApplicantsComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     this._nav.page = 'Applicants';
-    this._jobService
-      .getJobApplicants(+this._route.snapshot.paramMap.get('id'))
-      .subscribe((applicants: IApplication[]) => {
-        this.applicants = applicants;
-        this.dataSource = new MatTableDataSource(applicants);
-        this.dataSource.paginator = this.paginator;
+    this._route.paramMap.pipe(
+      switchMap((paramMap) => {
+        const jobId = +paramMap.get('id');
+        return this._jobService.getJobApplicants(jobId);
+      })
+    ).subscribe((applicants: IApplication[]) => {
+      this.applicants = applicants;
+      this.dataSource = new MatTableDataSource(applicants);
+      this.dataSource.paginator = this.paginator;
 
-        this._jobService.getSimilarityScores(applicants.map(value => value.id)).subscribe(
-          (similarityScores) => {
-            this.applicants.forEach((applicant, index) => {
-              const id = applicant.id;
-              if (similarityScores.hasOwnProperty(id)) {
-                applicant.similarityScore = similarityScores[id].score;
-              } else {
-                applicant.similarityScore = null;
-              }
-            });
-            this.dataSource = new MatTableDataSource(this.applicants);
-            this.dataSource.paginator = this.paginator;
-          },
-          (error) => {
-            // Handle error if needed
-            console.error('Error fetching similarity scores:', error);
-          }
-        );
-      });
+      this._jobService.getSimilarityScores(applicants.map(value => value.id)).subscribe(
+        (similarityScores) => {
+          this.applicants.forEach((applicant) => {
+            const id = applicant.id;
+            if (similarityScores.hasOwnProperty(id)) {
+              applicant.similarityScore =similarityScores[id].score;
+            } else {
+              applicant.similarityScore = NaN;
+            }
+          });
+          this.dataSource = new MatTableDataSource(this.applicants);
+          this.dataSource.paginator = this.paginator;
+        },
+        (error) => {
+          // Handle error if needed
+          console.error('Error fetching similarity scores:', error);
+        }
+      );
+    });
   }
+
   convertToPercentage(score: number|null): string {
     if (typeof score === 'number') {
       return Math.round(score * 100) + '%';
